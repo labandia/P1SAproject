a061a57074a6ac2790d06ec69c291107cb715199
labandia <65441114+labandia@users.noreply.github.com>
2025-06-11 10:19:34 +0800

    Folder fixed Struicture

--- ProgramPartListWeb/Services/EmailService.cs
+++ ProgramPartListWeb/Services/EmailService.cs
@@ -47,5 +47,33 @@ namespace ProgramPartListWeb.Utilities
                 return false;
             }
         }
+
+        public static void SendEmailOutlook(string stremail, string sub, string body)
+        {
+            Debug.WriteLine("HERE");
+
+            try
+            {
+                using (SmtpClient smtp = new SmtpClient("smtp.office365.com", 587))
+                {
+                    smtp.Credentials = new NetworkCredential("p1sa-processcontrol@sanyodenki.com", "p1sapc0325*");
+                    smtp.EnableSsl = true;
+
+                    MailMessage mail = new MailMessage();
+                    mail.From = new MailAddress("p1sa-processcontrol@sanyodenki.com");
+                    mail.To.Add(stremail);
+                    mail.Subject = sub;
+                    mail.Body = body;
+                    mail.IsBodyHtml = true;
+
+                    smtp.Send(mail);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Email could not be sent: " + ex.Message);
+            }
+        }
     }
+}
 }

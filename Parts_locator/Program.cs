b630fd4a680cc7a3cab4b067b0ffea66e1fb0abd
labandia <65441114+labandia@users.noreply.github.com>
2025-06-13 21:30:30 +0800

    fvfdvfdv

--- Parts_locator/Program.cs
+++ Parts_locator/Program.cs
@@ -1,6 +1,11 @@
 ﻿using Parts_locator.DataAccess;
+using Parts_locator.Interface;
+using Parts_locator.Modals;
 using Parts_locator.Models;
+using Parts_locator.Repository;
 using Parts_locator.View.Moldingbush;
+using Parts_locator.View.Moldingbush.Maincontent;
+using Parts_locator.View.Moldingbush.Modules;
 using Parts_locator.View.Rotor;
 using System;
 using System.Drawing;
@@ -11,6 +16,7 @@ namespace Parts_locator
 {
     internal static class Program
     {
+        public static IServiceProvider ServiceProvider { get; private set; }
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -20,8 +26,8 @@ namespace Parts_locator
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
 
             // Initialize your repositories.
             //IProductRepository ip = new SQLMainlayoutRespository();
@@ -38,7 +44,30 @@ namespace Parts_locator
             
 
 
-            Application.Run(new Startup());
+            //Application.Run(new Startup());
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            var services = new ServiceCollection();
+            services.AddSingleton<IRawMats, RawMatsRepository>();
+
+            services.AddSingleton<BushMain>();
+            services.AddSingleton<RawMaterialProductDetails>();
+            services.AddSingleton<Bushlocation>();
+            services.AddSingleton<BushMasterlist>();
+            services.AddSingleton<BushSummary_in>();
+            services.AddSingleton<BushSummary_out>();
+            services.AddSingleton<BushPartDetails>();
+            services.AddSingleton<EditMasterlist>();
+
+ 
+
+            ServiceProvider = services.BuildServiceProvider();
+            var mainForm = ServiceProvider.GetRequiredService<Startup>();
+            Application.Run(mainForm);
+
+
+
         }
     }   
 

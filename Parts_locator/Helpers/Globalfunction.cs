95944e4bfad1bf6b126a94f924861a0220feb928
labandia <65441114+labandia@users.noreply.github.com>
2025-06-15 21:00:53 +0800

    sdasdasdsa

--- Parts_locator/Helpers/Globalfunction.cs
+++ Parts_locator/Helpers/Globalfunction.cs
@@ -10,69 +10,11 @@ namespace Parts_locator.Data
     {
         public static int Pallet_number(string pname)
         {
-            // IMprove by list of array of string
-            int result = 0;
+            string[] palletNames = { "A", "B", "C", "D", "E", "F", "H", "I", "J", "L", "M", "N", "O", "P", "R", "S", "T", "U" };
 
-            switch (pname)
-            {
-                case "A":
-                    result = 1;
-                    break;
-                case "B":    
-                    result = 2; 
-                    break;
-                case "C":
-                    result = 3; 
-                    break;
-                case "D":                    
-                    result = 4; 
-                    break;
-                case "E":    
-                    result = 5; 
-                    break;                   
-                case "F":                   
-                    result = 6; 
-                    break;                      
-                case "H":          
-                    result = 7; 
-                    break;     
-                case "I":
-                    result = 8; 
-                    break;
-                case "J": 
-                    result = 9;
-                    break;
-                case "L":
-                    result = 10;
-                    break;
-                case "M":
-                    result = 11;
-                    break;
-                case "N":
-                    result = 12;
-                    break;
-                case "O":
-                    result = 13;
-                    break;
-                case "P":
-                    result = 14;
-                    break;
-                case "R":
-                    result = 15;
-                    break;
-                case "S":
-                    result = 16;
-                    break;
-                case "T":
-                    result = 17;
-                    break;
-                case "U":
-                    result = 18;
-                    break;
-            }
+            int index = Array.IndexOf(palletNames, pname);
 
-
-            return result;
+            return index >= 0 ? index + 1 : 0; // +1 to match your original numbering
         }
     }
 }

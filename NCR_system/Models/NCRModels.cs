7fdca001f4cd726055695018009aec48ed594aea
labandia <65441114+labandia@users.noreply.github.com>
2025-10-04 16:12:13 +0800

    NCR_project Added

---
+++ NCR_system/Models/NCRModels.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCR_system.Models
+{
+    public class NCRModels
+    {
+        private int _RecordID;
+        private string _DateIssued;
+        private string _RegNo;
+        private string _Category;
+        private string _IssueGroup;
+        private string _MainCharge;
+        private string _ModelNo;
+        private int _Quantity;
+        private string _Contents;
+        private int _CircularStatus;
+        private string _DateCloseReg;
+        private int _Status;
+
+
+        public int RecordID
+        {
+            get => _RecordID;
+            set => _RecordID = value;
+        }
+        public string DateIssued
+        {
+            get => _DateIssued;
+            set => _DateIssued = value;
+        }
+        public string RegNo
+        {
+            get => _RegNo;
+            set => _RegNo = value;
+        }
+        public string Category
+        {
+            get => _Category;
+            set => _Category = value;
+        }
+        public string IssueGroup
+        {
+            get => _IssueGroup;
+            set => _IssueGroup = value;
+        }
+        public string MainCharge
+        {
+            get => _MainCharge;
+            set => _MainCharge = value;
+        }
+        public string ModelNo
+        {
+            get => _ModelNo;
+            set => _ModelNo = value;
+        }
+        public int Quantity
+        {
+            get => _Quantity;
+            set => _Quantity = value;
+        }
+        public string Contents
+        {
+            get => _Contents;
+            set => _Contents = value;
+        }
+        public int CircularStatus
+        {
+            get => _CircularStatus;
+            set => _CircularStatus = value;
+        }
+        public string DateCloseReg
+        {
+            get => _DateCloseReg;
+            set => _DateCloseReg = value;
+        }
+        public int Status
+        {
+            get => _Status;
+            set => _Status = value;
+        }
+    }
+
+    public class CustomerModel
+    {
+        private int _RecordID;
+        private string _DateCreated;
+        private string _ModelNo;
+        private string _LotNo;
+        private int _NGQty;
+        private string _Details;
+        private string _RegNo;
+        private string _CustomerName;
+        private string _SIC;
+        private int _Status;
+
+
+        public int RecordID
+        {
+            get => _RecordID;
+            set => _RecordID = value;
+        }
+        public string DateCreated
+        {
+            get => _DateCreated;
+            set => _DateCreated = value;
+        }
+        public string ModelNo
+        {
+            get => _ModelNo;
+            set => _ModelNo = value;
+        }
+        public string LotNo
+        {
+            get => _LotNo;
+            set => _LotNo = value;
+        }
+        public int NGQty
+        {
+            get => _NGQty;
+            set => _NGQty = value;
+        }
+        public string Details
+        {
+            get => _Details;
+            set => _Details = value;
+        }
+        public string RegNo
+        {
+            get => _RegNo;
+            set => _RegNo = value;
+        }
+        public string CustomerName
+        {
+            get => _CustomerName;
+            set => _CustomerName = value;
+        }
+
+        public string SIC
+        {
+            get => _SIC;
+            set => _SIC = value;
+        } 
+        public int Status
+        {
+            get => _Status;
+            set => _Status = value;
+        }
+    }
+    public class InprocessModel
+    {
+        private int _RecordID;
+        private string _DateEncounter;
+        private string _TitleEmail;
+        private int _Shift;
+        private string _Line;
+        private string _Model;
+        private string _ShopOrder;
+        private string _Defect;
+        private int _NGQty;
+        private string _ProcEncounter;
+        private string _cause;
+        private int _section;
+        private string _Invest;
+        private int _Status;
+        private string _P1saStatus;
+        private string _Remarks;
+
+        public int RecordID
+        {
+            get => _RecordID;
+            set => _RecordID = value;
+        }
+        public string DateEncounter
+        {
+            get => _DateEncounter;
+            set => _DateEncounter = value;
+        }
+        public string TitleEmail
+        {
+            get => _TitleEmail;
+            set => _TitleEmail = value;
+        }
+        public int Shift
+        {
+            get => _Shift;
+            set => _Shift = value;
+        }
+        public string Line
+        {
+            get => _Line;
+            set => _Line = value;
+        }
+        public string Model
+        {
+            get => _Model;
+            set => _Model = value;
+        }
+        public string ShopOrder
+        {
+            get => _ShopOrder;
+            set => _ShopOrder = value;
+        }
+        public string Defect
+        {
+            get => _Defect;
+            set => _Defect = value;
+        }
+
+        public int NGQty
+        {
+            get => _NGQty;
+            set => _NGQty = value;
+        }
+        public string ProcEncounter
+        {
+            get => _ProcEncounter;
+            set => _ProcEncounter = value;
+        }
+        public string cause
+        {
+            get => _cause;
+            set => _cause = value;
+        }
+        public int section
+        {
+            get => _section;
+            set => _section = value;
+        }
+
+        public string Invest
+        {
+            get => _Invest;
+            set => _Invest = value;
+        }
+        public string P1saStatus
+        {
+            get => _P1saStatus;
+            set => _P1saStatus = value;
+        }
+
+        public string Remarks
+        {
+            get => _Remarks;
+            set => _Remarks = value;
+        }
+        public int Status
+        {
+            get => _Status;
+            set => _Status = value;
+        }
+    }
+
+    public class RejectShipmentModel
+    {
+        private int _RecordID;
+        private string _DateIssued;
+        private string _RegNo;
+        private string _Category;
+        private string _IssueGroup;
+        private string _MainCharge;
+        private string _ModelNo;
+        private int _Quantity;
+        private string _Contents;
+        private string _DateCloseReg;
+        private int _Status;
+        private int _Process;
+
+
+        public int RecordID
+        {
+            get => _RecordID;
+            set => _RecordID = value;
+        }
+        public string DateIssued
+        {
+            get => _DateIssued;
+            set => _DateIssued = value;
+        }
+        public string RegNo
+        {
+            get => _RegNo;
+            set => _RegNo = value;
+        }
+        public string Category
+        {
+            get => _Category;
+            set => _Category = value;
+        }
+        public string IssueGroup
+        {
+            get => _IssueGroup;
+            set => _IssueGroup = value;
+        }
+        public string MainCharge
+        {
+            get => _MainCharge;
+            set => _MainCharge = value;
+        }
+        public string ModelNo
+        {
+            get => _ModelNo;
+            set => _ModelNo = value;
+        }
+        public int Quantity
+        {
+            get => _Quantity;
+            set => _Quantity = value;
+        }
+        public string Contents
+        {
+            get => _Contents;
+            set => _Contents = value;
+        }
+        public string DateCloseReg
+        {
+            get => _DateCloseReg;
+            set => _DateCloseReg = value;
+        }
+        public int Process
+        {
+            get => _Process;
+            set => _Process = value;
+        }
+        public int Status
+        {
+            get => _Status;
+            set => _Status = value;
+        }
+    }
+
+}

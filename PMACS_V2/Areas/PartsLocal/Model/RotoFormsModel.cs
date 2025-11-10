96168bf44c2ad5d9d2e75a661cd5623f31066b88
labandia <65441114+labandia@users.noreply.github.com>
2025-11-10 18:27:44 +0800

    DDDDDDDDDDDDDDDD

--- PMACS_V2/Areas/PartsLocal/Model/RotoFormsModel.cs
+++ PMACS_V2/Areas/PartsLocal/Model/RotoFormsModel.cs
@@ -15,4 +15,27 @@ namespace PMACS_V2.Areas.PartsLocal.Model
         private string _FrontImage;
         private string _BackImage;
     }
+
+    public class ShopOrderInForm
+    {
+        public string ShopOrder { get; set; }
+
+        public int TransactionType { get; set; }
+        public string Partnumber { get; set; }
+        public int Quantity { get; set; } // --- Input Quantity
+        public string Area { get; set; }
+        public string Remarks { get; set; }
+
+    }
+
+    public class ShopOrderOutForm
+    {
+        public string ShopOrder { get; set; }
+        public int TransactionType { get; set; }
+        public string Partnumber { get; set; }
+        public int Quantity { get; set; }
+        public string Area { get; set; }
+        public string Remarks { get; set; }
+
+    }
 }

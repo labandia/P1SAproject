1624d1efaf7c256cf129430b8faff74bff8f2008
labandia <65441114+labandia@users.noreply.github.com>
2026-03-25 18:14:21 +0800

    adsadadas

--- POS_System/Pages/Form1.Designer.cs
+++ POS_System/Pages/Form1.Designer.cs
@@ -425,6 +425,7 @@
             // 
             // comboBox1
             // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Location = new System.Drawing.Point(717, 107);
             this.comboBox1.Name = "comboBox1";

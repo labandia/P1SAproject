07d2325f0a19a2cbf63a150d8c97b473f66abeb1
labandia <65441114+labandia@users.noreply.github.com>
2026-08-29 13:25:29 +0800

    asdsadsaasdsadsa

---
+++ ProgramPartListWeb/Areas/Production1/Controllers/DataAnalysisController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProgramPartListWeb.Areas.Production1.Controllers
+{
+    public class DataAnalysisController : Controller
+    {
+        // GET: Production1/DataAnalysis
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
